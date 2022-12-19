b882a8be297067afe5353b7af2c5b06dc5539cea
monalisa <monalisaganguly620@gmail.com>
2022-12-19 21:55:26 +0530

    2nd

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,1 +1,30 @@
 //!============================***** authantication *******===========================================//
+
+const jwt = require("jsonwebtoken");
+
+let auth = async function(req,res,next){
+
+try{
+    let bearerToken = req.headers.authorization
+    console.log(bearerToken)
+  
+    if(bearerToken){
+
+        token = bearerToken.split(" ")
+
+        let decodedToken = jwt.verify(token[1] ,  "fifth project" )      
+        if(decodedToken){
+
+       req.userId = decodedToken.userId
+        next()
+        
+    }
+
+    }else{ return res.status(401).send({ERROR:"Token Missing"})}   
+
+
+}catch(err){
+    return res.status(500).send({ERROR:err.message})}
+}
+
+module.exports.auth=auth
